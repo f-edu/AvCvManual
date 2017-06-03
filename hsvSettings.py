baffe5b366f539760f906b2beebe3bb312ff58d2
kuskov_stanislav <kuskov.stanislav@gmail.com>
2017-06-03 22:44:58 +0300

    manual of computer vision for autonomous vehicle (education) -traffic sign detection and recognition by pixels

--- hsvSettings.py
+++ hsvSettings.py
@@ -1,54 +1,54 @@
-import cv2
+import cv2 as cv
 import numpy as np
-cap = cv2.VideoCapture(0)
+cap = cv.VideoCapture(0)
 
 def nothing(x):
     pass
 # Creating a window for later use
-cv2.namedWindow('result')
+cv.namedWindow('result')
 
 # Starting with 100's to prevent error while masking
 h,s,v = 100,100,100
 mh,ms,mv = 100,100,100
 # Creating track bar
-cv2.createTrackbar('h', 'result',0,255,nothing)
-cv2.createTrackbar('s', 'result',0,255,nothing)
-cv2.createTrackbar('v', 'result',0,255,nothing)
+cv.createTrackbar('h', 'result',0,255,nothing)
+cv.createTrackbar('s', 'result',0,255,nothing)
+cv.createTrackbar('v', 'result',0,255,nothing)
 
-cv2.createTrackbar('mh', 'result',0,255,nothing)
-cv2.createTrackbar('ms', 'result',0,255,nothing)
-cv2.createTrackbar('mv', 'result',0,255,nothing)
+cv.createTrackbar('mh', 'result',0,255,nothing)
+cv.createTrackbar('ms', 'result',0,255,nothing)
+cv.createTrackbar('mv', 'result',0,255,nothing)
 
 while(1):
 
     _, frame = cap.read()
 
     #converting to HSV
-    hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    hsv = cv.cvtColor(frame,cv.COLOR_BGR2HSV)
 
     # get info from track bar and appy to result
-    h = cv2.getTrackbarPos('h','result')
-    s = cv2.getTrackbarPos('s','result')
-    v = cv2.getTrackbarPos('v','result')
+    h = cv.getTrackbarPos('h','result')
+    s = cv.getTrackbarPos('s','result')
+    v = cv.getTrackbarPos('v','result')
 
-    mh = cv2.getTrackbarPos('mh','result')
-    ms = cv2.getTrackbarPos('ms','result')
-    mv = cv2.getTrackbarPos('mv','result')
+    mh = cv.getTrackbarPos('mh','result')
+    ms = cv.getTrackbarPos('ms','result')
+    mv = cv.getTrackbarPos('mv','result')
 
     # Normal masking algorithm
     lower_blue = np.array([h,s,v])
     upper_blue = np.array([mh,ms,mv])
 
-    mask = cv2.inRange(hsv,lower_blue, upper_blue)
+    mask = cv.inRange(hsv,lower_blue, upper_blue)
 
-    result = cv2.bitwise_and(frame,frame,mask = mask)
+    result = cv.bitwise_and(frame,frame,mask = mask)
 
-    cv2.imshow('result',result)
+    cv.imshow('result',result)
 
-    k = cv2.waitKey(5) & 0xFF
+    k = cv.waitKey(5) & 0xFF
     if k == 27:
         break
 
 cap.release()
 
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
